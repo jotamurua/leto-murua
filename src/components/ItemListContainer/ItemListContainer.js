4a8ebd43ac55e207830a9a95a3461f3c8a5af9cd
jotamurua <102777812+jotamurua@users.noreply.github.com>
2022-07-13 18:37:54 -0300

    firebase completo
    firebase 1

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,13 +1,12 @@
 import React, { useEffect } from "react";
 import "./ItemListContainer.css"
 import { useState } from "react";
-import products from "../utils/products";
 import { ItemList } from "./ItemList/ItemList";
 import { SpinnerCircular } from 'spinners-react';
 import { useParams } from "react-router-dom";
 import { collection, getDocs, getFirestore, query, where } from "firebase/firestore";
 import app from '../../firebase/firebase'
-//import db from '../../firebase/firebase'
+
 
 
 export const ItemListContainer = () => {
@@ -23,48 +22,22 @@ export const ItemListContainer = () => {
       const db = getFirestore();
       
 
-      // const q = query(
-      //   collection(db, "procutos"),
-      //   where("category", "==", `${categoryid}`)
-      // );
+      const q = query(
+        collection(db, "productos"),
+        where("category", "==", `${categoryid}`)
+      );
 
-      // getDocs(q).then((snapshot) => {
-      //   if (snapshot.size === 0) {
-      //     console.log("No hay resultados");
-      //   }
-      // setItems(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data()})));
-      // });
       
       const itemsCollection = collection(db, "productos");
-      getDocs(itemsCollection).then((snapshot) => {
-        
+      getDocs(categoryid? q : itemsCollection).then((snapshot) => {
+        if (snapshot.size === 0) {
+          console.log("No hay resultados");
+        }  
         setItems(snapshot.docs.map((doc) => ({id: doc.id, ...doc.data() })));
         setSpinner(false)
       });
-  }, []);
-
-      //   const getItems = () => {
-      //   return new Promise((resolve, reject) => {
-      //     setTimeout (()=>{
-      //       if(products.length > 0){
-      //       resolve(products)} else {
-      //           reject ("ERROR!!!!")
-      //       }
-      //     }, 2000);
-          
-
-      //   });
-        
-      // }
-      
-      // getItems()
-      //   .then ((res) => categoryid? setItems(products.filter((product)=> product.category === `${categoryid}`)) : setItems((products)))
-      //   .catch ((err)=>{console.log(err)})
-      //   .finally(() => setSpinner(false))
-      // ;}, [categoryid])      
-        
-
-    
+  }, [categoryid]);
+  
     return (
         <div className="grid">
             <section>
