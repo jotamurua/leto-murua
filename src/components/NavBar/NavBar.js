774c89ca655274ab85bbdba11780d92213554fdd
jotamurua <102777812+jotamurua@users.noreply.github.com>
2022-07-24 11:44:27 -0300

    fix

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,14 +7,13 @@ import { CartWidget } from "./CartWidget/CartWidget";
 export const NavBar = () => {
     return <nav className="menu">
       <div className="boton">
-        <button>
+
         <NavLink to='/category/elab' className={({isActive}) => isActive? 'currentcategory' : ''}> <ion-icon name="water"></ion-icon></NavLink>
-        </button>
-        <button>
+        
         <NavLink to='/category/bean' className={({isActive}) => isActive? 'currentcategory' : ''}><ion-icon name="cafe"></ion-icon></NavLink>
-        </button>
+       
       </div>
-      <Link to="/"><button className="letra">LETO BREW LAB</button></Link>
+      <Link to="/" className="letra">LETO BREW LAB</Link>
       <ul className="menu-list">
         <li className="item"><NavLink to='/category/bean' className={({isActive}) => isActive? 'currentcategory' : ''}><button>CAFÉ</button></NavLink></li>
         <li className="item"><NavLink to='/category/elab' className={({isActive}) => isActive? 'currentcategory' : ''}><button>ELABORACIONES</button></NavLink></li>
