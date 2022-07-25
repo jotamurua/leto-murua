503b4c6c1d1b88f08d0010ef442c7b8c0edec7a7
jotamurua <102777812+jotamurua@users.noreply.github.com>
2022-07-25 16:36:21 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetail
 import { CartProvider } from './components/CartContext/CartContext';
 import { CartContainer } from './components/CartContainer/CartContainer';
 import { CartConfirm } from './components/CartContainer/CartConfirm/CartConfirm';
+import ErrorUrl from './components/ErrorUrl/ErrorUrl';
 
 function App() {
   return <>
@@ -18,7 +19,8 @@ function App() {
             <Route path="/item/:id" element={<ItemDetailContainer/>} />
             <Route path="/category/:categoryid" element={<ItemListContainer />} />
             <Route path="/cart" element={<CartContainer />} />
-            <Route path='/finish' element= {<CartConfirm />}></Route>
+            <Route path='/finish' element= {<CartConfirm />}/>
+            <Route path='/*' element= {<ErrorUrl/>}/>
           </Routes>
           </CartProvider>
           </BrowserRouter>
---
+++ src/components/ErrorUrl/ErrorUrl.js
@@ -0,0 +1,18 @@
+import './ErrorUrl.css'
+import { Link } from "react-router-dom";
+
+const ErrorUrl = () => {
+  return (
+    <section className='container-error'>
+      <div className='imagen-error'>
+        <img src='https://firebasestorage.googleapis.com/v0/b/murua-leto.appspot.com/o/Introducci%C3%B3n%20(2).png?alt=media&token=1b5ad41a-1ada-45c2-8e79-95e6669f1f00' alt='cafe con error 404'/>
+      </div>
+      <div className='texto-error'>
+        <h1>¿A dónde vas? Esto no existe</h1>
+        <Link to="/" className="link-component">Volver a inicio</Link>
+      </div>
+    </section>
+  )
+}
+
+export default ErrorUrl
